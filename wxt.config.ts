7a0565c11a6e6550c27d963d53891ef8a9e321c1
tojahech <tom@jjdat.com>
2025-11-30 19:17:41 +1000

    feat(): init with wxt+solid template

---
+++ wxt.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ["@wxt-dev/module-solid"],
+  manifest: {
+    commands: {
+      _execute_browser_action: {
+        suggested_key: {
+          default: "Ctrl+Space",
+        },
+      },
+    },
+  },
+});
